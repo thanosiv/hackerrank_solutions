13e34d83abc2cf7d6667356c4300e40f26f428bb
Charles Gibbs <47036193+thanosiv@users.noreply.github.com>
2022-09-30 15:15:01 -0400

    Update Draw The Triangle.sql

--- sql/Draw The Triangle/Draw The Triangle.sql
+++ sql/Draw The Triangle/Draw The Triangle.sql
@@ -2,15 +2,20 @@
 declare @totnum INT = 20;
 
 WHILE @totnum > 0
-BEGIN
-    DECLARE @countnum INT = @totnum
-    DECLARE @printstr VARCHAR(255) = '';
+    BEGIN
+        DECLARE @countnum INT = @totnum
+        DECLARE @printstr VARCHAR(255) = '';
     
-    WHILE @countnum > 0
-        BEGIN
-            SET @printstr += '* ';
-            SET @countnum -= 1;
-        END;
-    PRINT @printstr;
-    SET @totnum -= 1;
+        WHILE @countnum > 0
+            BEGIN
+                -- String MUST contain a space after each * char in order to meet the requirements
+                SET @printstr += '* ';
+                -- Subtract 1 from @countnum each loop til it gets to 0
+                SET @countnum -= 1;
+            END;
+        -- Print out the * character string 
+        PRINT @printstr;
+
+        -- Subtract 1 from the total number and start the loop again
+        SET @totnum -= 1;
 END;
